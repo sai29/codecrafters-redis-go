ef8b6b84cf1aa239f7d3d3790881ac2110840b8c
Sai Prashanth <saiprashanth93@gmail.com>
2024-12-06 12:53:18 +0530

    Send PONG response to client

--- app/server.go
+++ app/server.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -19,9 +15,11 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write([]byte("+PONG\r\n"))
+
 }
