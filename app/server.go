ee43da5c117eb6d5f244a6503ab3142a98af8857
Sai Prashanth <saiprashanth93@gmail.com>
2025-01-30 15:52:35 +0530

    Send PONG for multiple PINGS

--- app/server.go
+++ app/server.go
@@ -7,19 +7,41 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	conn.Write([]byte("+PONG\r\n"))
 
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Received data", buf[:n])
+
+	conn.Write(buf[:n])
 }
