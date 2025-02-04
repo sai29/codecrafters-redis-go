3db62a992f7406d47b97e8f17b554bda924d5f5a
Sai Prashanth <saiprashanth93@gmail.com>
2025-02-04 19:17:16 +0530

    Move listener outside for loop to accept multiple connections

--- app/server.go
+++ app/server.go
@@ -3,46 +3,33 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 )
 
 func main() {
-
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
-
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		fmt.Println("Error starting server:", err)
+		return
 	}
-
 	defer listener.Close()
+	fmt.Println("Server is listening on port 6379...")
 
-	fmt.Println("Server is listening on port 8080")
+	conn, err := listener.Accept()
 
 	for {
-
-		conn, err := listener.Accept()
-
 		if err != nil {
-			fmt.Println("Error: ", err)
 			continue
 		}
+		fmt.Println("New client connected")
 
-		conn.Write([]byte("+PONG\r\n"))
-		conn.Close()
+		handleConnection(conn)
 	}
 }
 
-func handleClient(conn net.Conn) {
-	defer conn.Close()
-
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
+func handleConnection(conn net.Conn) {
+	response := "+PONG\r\n"
+	_, err := conn.Write([]byte(response))
 	if err != nil {
-		return
+		fmt.Println("Error sending response:", err)
 	}
-
-	fmt.Println("Received data", buf[:n])
-
-	conn.Write([]byte("+PONG\r\n"))
 }
