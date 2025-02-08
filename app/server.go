4d93c6e29a1dbdcca3a0431993a49e9fb16914e8
Sai Prashanth <saiprashanth93@gmail.com>
2025-02-08 15:56:54 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
+type clientData struct {
+	activeClients atomic.Uint32
+}
+
 func main() {
+	c := &clientData{}
+	c.activeClients.Store(0)
+
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
@@ -15,22 +26,42 @@ func main() {
 	defer listener.Close()
 	fmt.Println("Server is listening on port 6379...")
 
-	conn, err := listener.Accept()
+	var wg sync.WaitGroup
 
 	for {
+		conn, err := listener.Accept()
+		c.activeClients.Add(1)
+		fmt.Println("Global counter is ", c.activeClients.Load())
 		if err != nil {
-			fmt.Println("Error in connection, ", err)
-			os.Exit(1)
+			fmt.Println("Error in lister.Accept() connection, ", err)
+			continue
 		}
-		handleConnection(conn)
+		wg.Add(1)
+		go func() {
+			handleConnection(conn, &wg, c)
+		}()
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		return
+func handleConnection(conn net.Conn, wg *sync.WaitGroup, c *clientData) {
+	defer wg.Done()
+	defer conn.Close()
+
+	conn.SetReadDeadline(time.Now().Add(5 * time.Minute))
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client disconnected.")
+			} else {
+				fmt.Println("Error in conn.Read()", err)
+			}
+			c.activeClients.Add(^uint32(0))
+			fmt.Println("Current active clients is ", c.activeClients.Load())
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
-	conn.Write([]byte("+PONG\r\n"))
 }
