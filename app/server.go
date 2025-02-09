03f09dfbf48fec9af299ba0d5e1ae5d19e56c55d
Sai Prashanth <saiprashanth93@gmail.com>
2025-02-09 06:00:01 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -24,7 +27,7 @@ func main() {
 		os.Exit(1)
 	}
 	defer listener.Close()
-	fmt.Println("Server is listening on port 6379...")
+	fmt.Println("server is listening on port 6379...")
 
 	var wg sync.WaitGroup
 
@@ -51,7 +54,8 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup, c *clientData) {
 
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		n, err := conn.Read(buf)
+
 		if err != nil {
 			if err == io.EOF {
 				fmt.Println("Client disconnected.")
@@ -62,6 +66,61 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup, c *clientData) {
 			fmt.Println("Current active clients is ", c.activeClients.Load())
 			return
 		}
-		conn.Write([]byte("+PONG\r\n"))
+		bufferString := string(buf[:n])
+
+		outputBytes, err := redisInputParser(bufferString)
+
+		if err != nil {
+			fmt.Println("error from redisInput parser", err)
+		} else {
+			conn.Write(outputBytes)
+		}
+
 	}
+}
+
+func redisInputParser(clientInput string) ([]byte, error) {
+	var input []string
+	output := make([]byte, 0, 256)
+
+	input = strings.Split(clientInput, "\r\n")
+	firstElement := input[0]
+
+	if strings.HasPrefix(firstElement, "*") {
+		crlfSeparatedValuesCount, _ := strconv.Atoi(firstElement[1:])
+		forCount := crlfSeparatedValuesCount * 2
+		currentCommand := ""
+
+		for i := 1; i <= forCount; i++ {
+
+			if strings.HasPrefix(input[i], "$") {
+				numberWithoutDollar := strings.ReplaceAll(input[i], "$", "")
+				_, err := strconv.Atoi(numberWithoutDollar)
+				if err != nil {
+					continue
+				}
+			} else {
+				switch strings.ToLower(input[i]) {
+				case "ping":
+					output = append(output, []byte("+PONG\r\n")...)
+					return []byte(output), nil
+				case "echo":
+					currentCommand = "echo"
+				default:
+
+					if currentCommand == "echo" {
+						respString := fmt.Sprintf("$%d\r\n%s\r\n", len(input[i]), input[i])
+						return []byte(respString), nil
+					} else {
+						return []byte{}, errors.New("invalid command")
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+	return []byte(output), nil
 }
