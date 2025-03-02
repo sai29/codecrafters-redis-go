ea18d2679b42952eaf418d38e63f35b562b786b0
Sai Prashanth <saiprashanth93@gmail.com>
2025-03-02 15:50:12 +0530

    RDB file - KEYS *

--- app/commands.go
+++ app/commands.go
@@ -1,12 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
+
+// redis-cli KEYS "*" - Get all redis keys from rdb file - only single key
+// redis-cli GET "foo" - Get a single value from the RDB file - string value
+// redis-cli KEYS "*" - Get all redis keys from rdb file - Multiple keys
+// redis-cli GET "foo",  redis-cli GET "bar" - Multiple keys at the same time hitting rdb file
+// redis-cli GET "foo",  redis-cli GET "bar" - Multiple keys at the same time hitting rdb file with expiry for some.
+
+type rdbStore struct {
+	store map[string]value
+}
+
+type rdbFileInfo struct {
+	currentDatabaseIndex int
+	totalHashCount       int
+	hashWithExpiryCount  int
+	currentByteCount     int
+}
+
+type rdbFileParser struct {
+	currentState int
+	// currentMode      int
+	currentKeyEncoding int
+	currentKeyLength   int
+	currentKey         string
+	currentValueLength int
+	currentValue       string
+	rdbFileInfo        rdbFileInfo
+}
+
+var stateChangeMap = map[int]string{
+	startState:               "startState",
+	metaDataState:            "metaDataState",
+	databaseSectionState:     "databaseSectionState",
+	databaseIndexState:       "databaseIndexState",
+	totalHashCountState:      "totalHashCountState",
+	hashWithExpiryCountState: "hashWithExpiryCountState",
+	keyEncodingState:         "keyEncodingState",
+	keyLengthState:           "keyLengthState",
+	keyParsingState:          "keyParsingState",
+	valueLengthState:         "valueLengthState",
+	valueParsingState:        "valueParsingState",
+	endOfFileState:           "endOfFileState",
+}
+
+const (
+	startState int = iota
+	metaDataState
+	databaseSectionState
+	databaseIndexState
+	totalHashCountState
+	hashWithExpiryCountState
+	keyEncodingState
+	keyLengthState
+	keyParsingState
+	valueLengthState
+	valueParsingState
+	endOfFileState
+)
+
+// const (
+// 	controlMode int = iota
+// 	sizeMode
+// 	keyValueMode
+// )
 
 func (r *redisStore) Set(args []string) bool {
 	r.mu.Lock()
@@ -48,9 +115,66 @@ func (r *redisStore) Get(key string) (string, error) {
 				return val.content, nil
 			}
 		}
-
 	}
 	return "", errors.New("err - no value for this key")
+}
+
+func (r *redisStore) Keys(args []string, config *config) (string, error) {
+	if args[0] == "*" {
+		path := config.rdb.dir + "/" + config.rdb.dbFileName
+		file, err := os.Open(path)
+		if err != nil {
+			fmt.Println("error opening file", err)
+			return "", err
+		}
+
+		defer file.Close()
+		rdbStore, _ := readFileForKeys(file)
+		return allKeysFromRdbStore(rdbStore)
+
+	}
+	return "", nil
+}
+
+func allKeysFromRdbStore(store rdbStore) (string, error) {
+	output := ""
+	output += fmt.Sprintf("*%d\r\n", len(store.store))
+	for key := range store.store {
+		output += fmt.Sprintf("$%d\r\n%s\r\n", len(key), key)
+	}
+	return output, nil
+}
+
+func readFileForKeys(file io.Reader) (rdbStore, error) {
+	reader := bufio.NewReader(file)
+	rdbParser := &rdbFileParser{currentState: startState, rdbFileInfo: rdbFileInfo{}}
+	rdbStore := &rdbStore{store: map[string]value{}}
+
+	buffer := make([]byte, 8*1024)
+
+	for {
+		rdbValCount, err := reader.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("end of buffer")
+				break
+			}
+			fmt.Println("Error while reading byte", err)
+		} else {
+			rdbParser.rdbFileInfo.currentByteCount += 1
+		}
+
+		for i := 0; i < rdbValCount; i++ {
+			// fmt.Printf("hex is: %X, string is: %s\n", buffer[i], string(buffer[i]))
+			currentState := rdbParser.currentState
+
+			rdbParser = getNextState(rdbParser, buffer, &i, rdbStore)
+			if currentState != rdbParser.currentState {
+				// fmt.Printf("State changes from current %s -> next %s\n", stateChangeMap[currentState], stateChangeMap[rdbParser.currentState])
+			}
+		}
+	}
+	return *rdbStore, nil
 }
 
 func (c *config) getRDBConfig(args []string) (string, error) {
@@ -64,6 +188,70 @@ func (c *config) getRDBConfig(args []string) (string, error) {
 		return respArrayGenerator(args[1], output), nil
 	}
 	return "", errors.New("err - unknown argument")
+}
+
+func getNextState(rdbParser *rdbFileParser, buffer []byte, i *int, rdbStore *rdbStore) *rdbFileParser {
+	b := buffer[*i]
+	switch rdbParser.currentState {
+	case startState:
+		if b == 0xFA {
+			rdbParser.currentState = metaDataState
+		}
+	case metaDataState:
+		if b == 0xFE {
+			rdbParser.currentState = databaseSectionState
+		}
+	case databaseSectionState:
+		rdbParser.rdbFileInfo.currentDatabaseIndex = int(b)
+		rdbParser.currentState = databaseIndexState
+	case databaseIndexState:
+		if b == 0xFB {
+			rdbParser.currentState = totalHashCountState
+		}
+	case totalHashCountState:
+		rdbParser.rdbFileInfo.totalHashCount = int(b)
+		rdbParser.currentState = hashWithExpiryCountState
+	case hashWithExpiryCountState:
+		rdbParser.rdbFileInfo.hashWithExpiryCount = int(b)
+		rdbParser.currentState = keyEncodingState
+	case keyEncodingState:
+		rdbParser.currentKeyEncoding = int(b)
+		rdbParser.currentState = keyLengthState
+	case keyLengthState:
+		rdbParser.currentKeyLength = int(b)
+		rdbParser.currentState = keyParsingState
+	case keyParsingState:
+		rdbParser.currentKey = getBufferValue(rdbParser.currentKeyLength, buffer, i)
+		rdbParser.currentState = valueLengthState
+	case valueLengthState:
+		rdbParser.currentValueLength = int(b)
+		rdbParser.currentState = valueParsingState
+	case valueParsingState:
+		if buffer[*i] == 0xFF {
+			rdbParser.currentState = endOfFileState
+			rdbStore.store[rdbParser.currentKey] = value{
+				content: rdbParser.currentValue,
+				expiry:  0,
+			}
+			rdbParser.currentKey = ""
+			rdbParser.currentValue = ""
+			break
+		}
+
+		rdbParser.currentValue = getBufferValue(rdbParser.currentValueLength, buffer, i)
+	}
+	return rdbParser
+}
+
+func getBufferValue(keyLength int, buffer []byte, i *int) string {
+	if *i+keyLength < len(buffer) {
+		currentKey := string(buffer[*i : *i+keyLength])
+		*i += keyLength - 1
+		return currentKey
+	} else {
+		// For now.
+		return ""
+	}
 }
 
 func handleCommand(command string, args []string, store *redisStore, config *config) (string, error) {
@@ -91,6 +279,8 @@ func handleCommand(command string, args []string, store *redisStore, config *con
 		return fmt.Sprintf("$%d\r\n%s\r\n", len(string), string), nil
 	case "config":
 		return config.getRDBConfig(args)
+	case "keys":
+		return store.Keys(args, config)
 	default:
 		return "", errors.New("err - unknown command")
 	}
